5ecfd84d16fde01a776c9613fdcf693398a69cfa
Burak CAN <drcan94@gmail.com>
2022-12-06 09:24:59 +0300

    finally carousel shifting problem is solved

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -6,18 +6,24 @@ import {MantineTheme, useMantineTheme} from "@mantine/core";
 import {useMediaQuery, useElementSize} from '@mantine/hooks';
 
 const ImageCarousel: React.FC<ImagesTypes> = ({images, width, height, isOpen}) => {
-    const autoplay = useRef(Autoplay({delay: 4000}));
+    const autoplay = useRef(Autoplay({delay: 1700}));
     const theme: MantineTheme = useMantineTheme();
     const {ref: imageRef, width: imageWidth} = useElementSize();
+
     const TRANSITION_DURATION = 200;
+
     const [embla, setEmbla] = useState<Embla | null>(null);
     useAnimationOffsetEffect(embla, TRANSITION_DURATION);
+
     const xs = useMediaQuery(`(max-width: ${theme.breakpoints.xs}px)`);
     const sm = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
     const md = useMediaQuery(`(max-width: ${theme.breakpoints.md}px)`);
     const lg = useMediaQuery(`(max-width: ${theme.breakpoints.lg}px)`);
-
     // const xl = useMediaQuery(`(max-width: ${theme.breakpoints.xl}px)`);
+
+    useEffect(() => {
+        embla?.reInit()
+    }, [imageWidth, embla])
 
     return (
         <Carousel
@@ -26,7 +32,7 @@ const ImageCarousel: React.FC<ImagesTypes> = ({images, width, height, isOpen}) =
             withIndicators
             dragFree={false}
             loop
-            slideGap={"md"}
+            slideGap={xs ? "xs" : sm ? "sm" : md ? "md" : lg ? "lg" : "xl"}
             align="center"
             plugins={[autoplay.current]}
             onMouseEnter={autoplay.current.stop}
@@ -36,13 +42,13 @@ const ImageCarousel: React.FC<ImagesTypes> = ({images, width, height, isOpen}) =
                     width: imageWidth,
                 },
                 slide: {
-                    width: "100%",
                     display: "flex",
                     justifyContent: "center"
                 },
                 container: {
                     display: "flex",
                 },
+
                 indicator: {
                     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.blue[4],
                     width: 12,
@@ -60,15 +66,10 @@ const ImageCarousel: React.FC<ImagesTypes> = ({images, width, height, isOpen}) =
                     <Carousel.Slide key={idx}>
                         <img ref={imageRef} style={{
                             width:
-                            // xs ? 300
-                            //     : sm ? theme.breakpoints.xs
-                            //         : md ? theme.breakpoints.sm - 200
-                            //             : lg ? theme.breakpoints.md - 200 : 1000,
                                 xs ? theme.breakpoints.xs - 150
                                     : sm ? (!isOpen ? (theme.breakpoints.sm - 220 - 150) : (theme.breakpoints.sm - 150))
                                         : md ? (isOpen ? (theme.breakpoints.md - 220 - 250) : (theme.breakpoints.md - 250))
                                             : lg ? (isOpen ? (700) : (920)) : (1000),
-                            // with this conditional (depend on "isOpen") css, carousel is not working properly ?
                             objectFit: "contain",
                         }}
                              src={image.imageUrl} alt={image.imageAlt}
